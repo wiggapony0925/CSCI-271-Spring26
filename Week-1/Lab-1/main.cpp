e59f1e2ca3fd25ac8fa1ce51e8f0b8943e72c92c
JFMCaptialGroupLLC <capital.jfm@gmail.com>
2026-02-04 20:29:26 -0500

    Add Week 1 Lab 1: Mini Lab

---
+++ Week-1/Lab-1/main.cpp
@@ -0,0 +1,21 @@
+// Jeffrey Fernandez
+// Lab 1 - Mini Lab
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string name;
+  int age;
+
+  cout << "Enter name: ";
+  cin >> name;
+
+  cout << "Enter age: ";
+  cin >> age;
+
+  cout << "Hello, I am " << name << "! I am " << age << " year old." << endl;
+
+  return 0;
+}
